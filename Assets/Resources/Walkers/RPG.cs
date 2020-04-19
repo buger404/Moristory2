f9b5672289b51cdd4f49ca424850630ff3151e4a
ris_vb@126.com <ris_vb@126.com>
2020-04-19 18:23:29 +0800

    扩展更多脚本指令，移除旧的脚本解释器

--- Assets/Resources/Walkers/RPG.cs
+++ Assets/Resources/Walkers/RPG.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Xml;
 using UnityEngine;
 
+/// <summary>
+/// 代码状态
+/// </summary>
 public class RunState{
-    public RunState next;
-    public RunState prev;
-    public int line;
-    public XmlNodeList code;
-    public XmlNode root;
+    public RunState next; //if环的下一层
+    public RunState prev; //if环的上一层
+    public int line; //当前层的代码执行到的行号
+    public XmlNodeList code; //代码集合
+    public XmlNode root; //父代码
     public RunState(){
         prev = null;next = null;line = 0;
     }
@@ -21,77 +24,176 @@ public class RunState{
 }
 public class RPG : MonoBehaviour
 {
+    /// <summary>
+    /// 储存代码xml源文本
+    /// </summary>
     public TextAsset Code;
+    /// <summary>
+    /// 当前对话的角色名
+    /// </summary>
     private string Character = "";
+    /// <summary>
+    /// 储存代码xml
+    /// </summary>
+    /// <returns></returns>
     private XmlDocument xml = new XmlDocument();
+    /// <summary>
+    /// 是否有别的behaviour正在执行
+    /// </summary>
     private bool IsRunning = false;
+    /// <summary>
+    /// 代码运行状态
+    /// </summary>
+    /// <returns></returns>
     private RunState rs = new RunState();
+
+    //挂起游戏部分操作
     public void HangUp(){
+        //挂起游戏的部分操作
         GameConfig.IsBlocking = true;
         GameConfig.BlockEvent = this;
         Debug.Log("Behave hang up");
     }
+
+    //每行代码的执行
     public void Run(){
+        //--代码执行准备-----------------------------------------------------
         RunState r = rs, pr = rs;
         XmlElement c = (XmlElement)r.code[r.line];
+        //深入当前执行的最内层，取得当前执行的代码
         while(r != null){
             recode:
+            //当前层代码执行完毕
             if(r.line >= r.code.Count){
                 if(r.prev == null) {
+                    //如果是最上层代码执行完毕，说明该behaviour执行结束
+                    //关闭当前开启的对话框
                     if(GameConfig.IsMsgProcess){GameConfig.ActiveDialog.EndMsg();}
                     IsRunning = false;
                     return;
                 }
+                //删除该层
                 r.prev.NextLine();
                 r.prev.next = null;
                 r = r.prev;
                 goto recode;
             }
             c = (XmlElement)r.code[r.line];
+            //如果该代码属于该层
             if(c.ParentNode != r.root){
                 r.NextLine();goto recode;
             }
             pr = r;r = r.next;
         }
-        
-        bool MsgProccessed = false;
-        bool ExitMark = false;
-        bool BlockCode = false;
+        bool MsgProccessed = false;     //是否有对话框执行的操作
+        bool ExitMark = false;          //是否有立即退出behaviour的操作
+        bool BlockCode = false;         //是否暂停运行下一行代码
+        //临时变量
+        RPGEvent rpg = this.gameObject.GetComponent<RPGEvent>();
+        GameObject go = this.gameObject;
 
-        if(c.Name == "var"){
-            Storage.Set(c.GetAttribute("tar"),Storage.Condition(c.InnerText).ToString());
-        }
-        if(c.Name == "chara"){
-            Character = c.InnerText;
-        }
+
+
+        //--代码本体的关键操作-----------------------------------------------
+        //套if层到当前层的next
         if(c.Name == "if"){
+            //判断时候满足if条件
             if((bool)Storage.Condition(c.GetAttribute("condition"))){
                 Debug.Log("enter to a new if");
                 pr.Append(c.GetElementsByTagName("*"),c);
                 Run();return;
             }
         }
+        //退出behaviour
+        if(c.Name == "exit"){
+            ExitMark = true;
+        }
+
+
+        //--变量操作--------------------------------------------------------
+        //游戏变量操作
+        if(c.Name == "var"){
+            Storage.Set(c.GetAttribute("tar"),Storage.Condition(c.InnerText).ToString());
+        }
+        
+
+        //--对话框操作------------------------------------------------------
+        //设置当前对话角色
+        if(c.Name == "chara"){
+            Character = c.InnerText;
+        }
+        //发起对话框的对话
         if(c.Name == "s"){
             GameConfig.ActiveDialog.CreateMsg(Character,Storage.Deepin(c.InnerText));
+            //阻止代码继续运行直到对话框被用户关闭
             BlockCode = true;MsgProccessed = true;
         }
 
+
+        //--人物操作-------------------------------------------------------
+        //设置行走任务
+        if(c.Name == "walk"){
+            if(c.GetAttribute("tar") != null)
+            rpg = GameObject.Find(c.GetAttribute("tar")).GetComponent<RPGEvent>();
+            if(c.GetAttribute("x") != null)
+            rpg.XTask = float.Parse(Storage.Condition(c.GetAttribute("x")).ToString());
+            if(c.GetAttribute("y") != null)
+            rpg.YTask = float.Parse(Storage.Condition(c.GetAttribute("y")).ToString());
+        }
+        //开始根据设定的任务行走
+        if(c.Name == "walktask"){
+            //等待走完
+            BlockCode = true;GameConfig.WalkingTask = true;
+        }
+        //和指定目标对齐坐标
+        if(c.Name == "fix"){
+            Vector3 otrans = this.gameObject.transform.position;
+            Transform ttrans = GameObject.Find(c.GetAttribute("src")).transform;
+            if(c.GetAttribute("tar").IndexOf("x") >= 0){
+                ttrans.position = new Vector3(otrans.x,ttrans.position.y,ttrans.position.z);
+            }
+            if(c.GetAttribute("tar").IndexOf("y") >= 0){
+                ttrans.position = new Vector3(ttrans.position.x,otrans.y,ttrans.position.z);
+            }
+        }
+        //显示或隐藏目标
+        if(c.Name == "visible"){
+            if(c.GetAttribute("tar") != null)  go = GameObject.Find(c.GetAttribute("tar"));
+            go.SetActive((bool)Storage.Condition(c.InnerText));
+        }
+
+
+        //--地图操作-------------------------------------------------------
+        //昼夜开关
+        if(c.Name == "time"){
+            if(c.InnerText == "day") GameConfig.DayNight = 0;
+            if(c.InnerText == "night") GameConfig.DayNight = 1;
+        }
+
+
+        //--代码执行收尾----------------------------------------------------
+        //若对话框没有被使用，则当作废弃关闭
         if(!MsgProccessed){
             if(GameConfig.IsMsgProcess){GameConfig.ActiveDialog.EndMsg();}
         }
+        //需要立即退出behaviour
         if(ExitMark){
             IsRunning = false; return;
         }
-
+        //跳转到下一行
         pr.NextLine();
-        if(!BlockCode){Run();}
-        if(BlockCode){HangUp();}
+        //是否挂起
+        if(!BlockCode){Run();}else{HangUp();}
     }
+
+    //Behaviour装载准备
     public void Begin(string behave){
+        //如果当前有别的behaviour在执行，则退出
         if(IsRunning) return;
 
         XmlNodeList behaviours = xml.GetElementsByTagName("behaviour");
         foreach(XmlElement behaviour in behaviours){
+            //判断是否是请求的behaviour，并判断是否满足其要求的条件
             if(behaviour.GetAttribute("action") == behave &&
                (bool)Storage.Condition(behaviour.GetAttribute("condition"))
                )
@@ -105,25 +207,31 @@ public class RPG : MonoBehaviour
     }
 
     private void Start() {
+        //装载代码xml
         xml.LoadXml(Code.text);
+        //调用auto behaviour
         Begin("auto");
     }
 
     private void Update() {
+        //如果最后一个进入碰撞的RPG是自己
         if(GameConfig.LastEvent == this){
+            //支持的触发输入
             if(Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Z)){
-                Begin("spy");
+                Begin("spy"); //调用spy behaviour
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        //当玩家进入碰撞区域时，开始允许玩家触发spy behaviour
         GameConfig.LastEvent = this;
     }
     private void OnTriggerExit2D(Collider2D other) {
         GameConfig.LastEvent = null;
     }
     private void OnTriggerStay2D(Collider2D other) {
+        //接触即调用touch behaviour
         Begin("touch");
     }
 }
--- Assets/Storage.cs
+++ Assets/Storage.cs
@@ -13,15 +13,41 @@ public class Storage
     public static void Set(string name,string value){
         PlayerPrefs.SetString(name,value);
     }
+    /// <summary>
+    /// 计算表达式
+    /// </summary>
+    /// <param name="expression">表达式</param>
+    /// <returns>结算结果</returns>
     public static object Compute(string expression){
         return dt.Compute(expression.Replace("!=","<>"), null);
     }
+    /// <summary>
+    /// 根据存档数据格式化后计算表达式
+    /// </summary>
+    /// <param name="expression">源表达式</param>
+    /// <returns>计算结果</returns>
     public static object Condition(string expression){   
         return Compute(Deepin(expression));
     }
+    /// <summary>
+    /// 将文本使用存档数据格式化
+    /// </summary>
+    /// <param name="expression">源文本</param>
+    /// <returns>格式化结果</returns>
     public static string Deepin(string expression){
-        string[] t = expression.Replace("]","[").Split('[');
-        string s = "";
+        string s = expression;
+        //常量替换
+        s = s.Replace("[step]","1.435");
+        s = s.Replace("FACE_RIGHT","2");
+        s = s.Replace("FACE_LEFT","1");
+        s = s.Replace("FACE_UP","3");
+        s = s.Replace("FACE_DOWN","0");
+        if(GameConfig.Controller != null){
+            s = s.Replace("[face]",GameConfig.Controller.GetComponent<RPGEvent>().Direction.ToString());
+        }
+
+        //根据存档替换
+        string[] t = s.Replace("]","[").Split('[');
         for(int i = 0;i < t.Length;i++){
             if(i%2 == 0){
                 s += t[i];
