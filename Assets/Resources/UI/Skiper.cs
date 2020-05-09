dbbd1900f8aa6dfeae6a3d3f1134e0e526e6ec1a
ris_vb@126.com <ris_vb@126.com>
2020-05-09 22:44:26 +0800

    修复Skiper错误 为传说添加跳过按钮

--- Assets/Resources/UI/Skiper.cs
+++ Assets/Resources/UI/Skiper.cs
@@ -15,19 +15,17 @@ public class Skiper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Space)){
-            if(SkipTo == "NameTime"){
-                if(PlayerPrefs.GetString("scenecode") != ""){
-                    Switcher.SwitchTo("Saving");
-                }else{
-                    Switcher.SwitchTo(SkipTo);
-                }
-            }else{
-                if(Input.GetMouseButtonUp(0)){
-                    foreach(RaycastHit2D hit in Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero)){
-                        if(hit.transform.name.StartsWith("Skip")){
+        if(Input.GetMouseButtonUp(0)){
+            foreach(RaycastHit2D hit in Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero)){
+                if(hit.transform.name.StartsWith("Skip")){
+                    if(SkipTo == "NameTime"){
+                        if(PlayerPrefs.GetString("scenecode") != ""){
+                            Switcher.SwitchTo("Saving");
+                        }else{
                             Switcher.SwitchTo(SkipTo);
                         }
+                    }else{
+                        Switcher.SwitchTo(SkipTo);
                     }
                 }
             }
